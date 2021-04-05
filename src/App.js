6f5b276eebe3c491c6321f09a5773a8f4e5fc6b2
Matt Lindly <matt@arrayschool.com>
2021-04-04 22:55:37 -0600

    Initial repositories are set up for students.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        {/* Begin writing code for your website... */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
